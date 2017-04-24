ac3d1bf7ab88ca1f74aba5871d75473aee2abf6d
Thomas Van Doren <thomas.vandoren@gmail.com>
2017-04-24 08:34:49 -0700

    more tinkering... launching does not quite work from docker

--- runtime/src/launch/aws-ec2/chapel-launcher-aws-ec2/ec2_cloud.py
+++ runtime/src/launch/aws-ec2/chapel-launcher-aws-ec2/ec2_cloud.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import boto3
+import cStringIO
 from ec2_instance import EC2Instance
 from ec2_launchable_instance import EC2LaunchableInstance
 import logging
 import os
 import os.path
 import paramiko.client
+import posixpath
 
 LOG = logging.getLogger('chapel_aws_ec2')
 
@@ -73,7 +75,10 @@ class EC2Cloud(object):
             instances = instances[0:max_number]
         return instances
 
-    def copy_to_instances(self, chapel_exe, instances, key_pair_name, key_pair_key):
+    def _get_ip(self, instance):
+        return instance.public_ip
+
+    def copy_to_instances(self, chapel_exe, instances, key_pair_name, key_pair_key, ssh_user):
         """Copies chapel executable file to all instances.
 
         :param chapel_exe: Chapel executable file path
@@ -84,9 +89,16 @@ class EC2Cloud(object):
         """
         base_chapel_exe = chapel_exe
 
+        ssh_config = ''
+        for instance in instances:
+            ip = self._get_ip(instance)
+            ssh_config += 'Host {}\n'.format(ip)
+            ssh_config += '\tIdentityFile {}\n\n'.format(key_pair_key)
+        ssh_config_file = cStringIO.StringIO(ssh_config)
+
         results = []
         for instance in instances:
-            ip = instance.public_ip
+            ip = self._get_ip(instance)
 
             with paramiko.client.SSHClient() as ssh:
 
@@ -101,30 +113,65 @@ class EC2Cloud(object):
                 ssh.connect(
                     hostname=ip,
                     port=22,
-                    username='ec2-user',
+                    username=ssh_user,
                     key_filename=key_pair_key
                 )
 
                 sftp = ssh.open_sftp()
-                response = sftp.put(
+                response1 = sftp.put(
                     chapel_exe,
                     base_chapel_exe,
                     confirm=True
                 )
                 sftp.chmod(base_chapel_exe, 0700)
 
-                # FIXME: validate checksum of uploaded file
+                # FIXME: find a better way to do this.
+                home_dir = sftp.normalize('.')
+                LOG.debug('Home directory is %s on %s', home_dir, instance)
+
+                ssh_dir = posixpath.join(home_dir, '.ssh')
+                LOG.debug('Creating directory %s on %s', ssh_dir, instance)
+                try:
+                    sftp.mkdir(ssh_dir, 0700)
+                except IOError:
+                    LOG.debug('Failed to create directory %s, assuming it exists on %s', ssh_dir, instance)
+
+                ssh_config_filename = posixpath.join(ssh_dir, 'config')
+                LOG.debug('Writing ssh config to: %s on %s', ssh_config_filename, instance)
+                sftp.putfo(
+                    ssh_config_file,
+                    ssh_config_filename,
+                    confirm=True
+                )
+                sftp.chmod(ssh_config_filename, 0644)
+
+                # FIXME: validate checksum of uploaded file(s)
 
                 results.append(EC2LaunchableInstance(instance, base_chapel_exe))
 
         LOG.info('Copied %s to %d instances.', chapel_exe, len(results))
         return results
 
-    def run_on_instances(self, launchable_instances, chapel_args, key_pair_name, key_pair_key):
-        cmd_args = ' '.join(chapel_args)
+    def run_on_instances(self, launchable_instances, chapel_exe, chapel_args, key_pair_name, key_pair_key, ssh_user):
+        all_ips = [self._get_ip(li.instance) for li in launchable_instances]
+
+        env = os.environ.copy()
+        env['GASNET_SPAWNFN'] = 'S'
+        env['GASNET_MASTERIP'] = all_ips[0]
+        env['GASNET_SSH_SERVERS'] = ' '.join(all_ips)
+        env['GASNET_SSH_REMOTE_PATH'] = '/home/ec2-user' # FIXME: what should this be?
+        env['GASNET_SSH_OPTIONS'] = '-i {} -l {}'.format(key_pair_key, ssh_user)
+
+        import subprocess
+        cmd = [chapel_exe] + list(chapel_args)
+        LOG.debug('Executing command: %s', cmd)
+        subprocess.check_call(cmd, env=env)
+
+
+        return False
         results = []
         for instance in launchable_instances:
-            ip = instance.instance.public_ip
+            ip = self._get_ip(instance.instance)
 
             with paramiko.client.SSHClient() as ssh:
 
--- runtime/src/launch/aws-ec2/chapel-launcher-aws-ec2/launcher.py
+++ runtime/src/launch/aws-ec2/chapel-launcher-aws-ec2/launcher.py
@@ -16,16 +16,17 @@ LOG = logging.getLogger(LOG_NAME)
 @click.option('--instance-name')
 @click.option('--key-pair-name')
 @click.option('--key-pair-key', type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True))
+@click.option('--ssh-user', default='ec2-user')
 @click.argument('chapel_exe', nargs=1, type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True))
 @click.argument('chapel_args', nargs=-1, type=click.STRING)
-def cli(num_locales, instance_name, key_pair_name, key_pair_key, chapel_exe, chapel_args):
+def cli(num_locales, instance_name, key_pair_name, key_pair_key, ssh_user, chapel_exe, chapel_args):
     LOG.debug('chapel executable is: "%s": with arguments: %s', chapel_exe, chapel_args)
 
     ec2 = EC2Cloud()
 
     instances = ec2.get_instances_by_name(instance_name, max_number=num_locales)
-    launchable_instances = ec2.copy_to_instances(chapel_exe, instances, key_pair_name, key_pair_key)
-    ec2.run_on_instances(launchable_instances, chapel_args, key_pair_name, key_pair_key)
+    launchable_instances = ec2.copy_to_instances(chapel_exe, instances, key_pair_name, key_pair_key, ssh_user)
+    ec2.run_on_instances(launchable_instances, chapel_exe, chapel_args, key_pair_name, key_pair_key, ssh_user)
 
 
 
